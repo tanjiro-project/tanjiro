31a18f0903a93fece6120d05abc62777c2dbd500
KagChi <yonuxbot@gmail.com>
2024-10-26 20:23:28 +0700

    fix: correctly run the migrations

--- database/src/main.rs
+++ database/src/main.rs
@@ -5,7 +5,7 @@ use std::env;
 static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
